ea1b69059130e9a9cd234fdd06fea2c66c64dd2a
Christian Turner <47116506+cturner0098@users.noreply.github.com>
2020-02-20 13:01:01 -0500

    Added Functionality
    Started adding features to recieve the files in R22/scripts, currently not functional.
    
    ServerStart is hard coded into R22 server, looking to change that to work in the scripts folder.

--- server/R22/main.py
+++ server/R22/main.py
@@ -1,19 +1,52 @@
 import socket
+import os
 
-LOCALHOST = "10.230.209.87"
+LOCALHOST = socket.gethostbyname(socket.gethostname())
 PORT = 8080
 
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server.bind((LOCALHOST, PORT))
 server.listen(1)
 
-print("Server Started")
-print("Waiting for client request...")
+PATH = os.getcwd()
+files = []
 
-clientConnection, clientAddress = server.accept()
+for r, d, f in os.walk(PATH):
+    for file in f:
+        if '.sh' in file:
+            files.append(os.path.join(r, file))
 
-while True:
-    print("Connected client: ", clientAddress)
-    data = clientConnection.recv(1024)
-    print("From client: ", data.decode())
-    clientConnection.send(bytes("Successfully connected to server!", 'UTF-8'))
+
+def wait_for_connection():
+    print("Waiting for client request...")
+    client_connection, client_address = server.accept()
+
+    while True:
+        try:
+            print("Connected client: ", client_address)
+            data = client_connection.recv(1024)
+
+            if data.decode().strip() == "get_scripts()":
+                get_scripts(client_connection)
+
+            if data.decode().strip() == "start":
+                os.system("cd ~/texxit && screen -dmS texxit ./ServerStart.sh")
+                print("Texxit Server Started?")
+
+            print("From client: ", data.decode())
+        except:
+            print("User Disconnected")
+            client_connection.close()
+            wait_for_connection()
+
+
+def get_scripts(client_connection):
+    print("Getting Scripts...")
+    for file in files:
+        client_connection.sendall(file)
+
+
+
+if __name__ == "__main__":
+    print("Server Started")
+    wait_for_connection()
--- server/venv/Scripts/activate.bat
+++ server/venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\cturn\PycharmProjects\R22\venv
+set VIRTUAL_ENV=C:\Users\cturn\Documents\GitHub\R22\server\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
